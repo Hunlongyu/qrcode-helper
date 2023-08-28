9585f22099a5e8e07bfcd59bbdc4cecc850a6673
hunlongyu <hunlongyu@gmail.com>
2023-08-28 23:22:28 +0800

    feat: 升级 qcsc ，并将剪贴板转成temp目录

--- src-tauri/src/parse.rs
+++ src-tauri/src/parse.rs
@@ -1,11 +1,14 @@
-use arboard::Clipboard;
 use async_process::Command;
 use image::load_from_memory;
 use rqrr::PreparedImage;
 use rxing;
 use screenshots::Screen;
 use serde::{Deserialize, Serialize};
-use std::{collections::HashSet, vec};
+use std::fs;
+use std::path::PathBuf;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::{collections::HashSet, env, vec};
+
 #[derive(Serialize, Deserialize, Clone)]
 struct Result {
     result: bool,
@@ -14,6 +17,7 @@ struct Result {
 
 // 解析多个文件，并可以指定 解析库 来解析二维码图片
 pub async fn parse_images_with_paths(paths: String, lib: Option<String>) -> String {
+    println!("paths: {}", paths);
     let paths_json: Vec<String> = serde_json::from_str(&paths).unwrap();
     let mut tasks = vec![];
     for path in paths_json {
@@ -142,30 +146,35 @@ pub fn scan_screen() -> String {
 
 // 鼠标截图保存 temp 目录下，并进行识别
 pub async fn screen_capture() -> String {
-    let mut child = Command::new("G:\\Github\\qrcode-helper\\src-tauri\\libs\\sc.exe")
+    let temp_dir = env::temp_dir();
+    println!("temp_dir: {:?}", temp_dir);
+
+    let temp_dir_long = std::fs::canonicalize(&temp_dir).unwrap();
+    println!("temp_dir_long: {:?}", temp_dir_long);
+
+    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let seconds = timestamp.as_secs();
+
+    let mut file_name = PathBuf::from(temp_dir_long);
+    file_name.push("qrcode-helper");
+    file_name.push(seconds.to_string());
+    file_name.set_extension("png");
+    println!("file_name: {:?}", file_name);
+
+    if let Some(parent) = file_name.parent() {
+        fs::create_dir_all(parent).expect("Failed to create directories");
+    }
+
+    let mut child = Command::new("E:\\qrcode-helper\\src-tauri\\libs\\qcsc.exe")
+        .arg(&file_name)
+        // .arg("C:\\Users\\hunlongyu\\AppData\\Local\\Temp\\012\\1619784753.png")
         .spawn()
         .unwrap();
     let _ = child.status().await.unwrap();
 
-    let mut clipboard = Clipboard::new().unwrap();
-    let img_ = clipboard.get_image().unwrap();
-    let v_img = img_.bytes.to_vec();
-    let mut results = Vec::new();
-    let grids_arr =
-        rxing::helpers::detect_multiple_in_luma(v_img, img_.width as u32, img_.height as u32);
-    if let Err(_) = grids_arr {
-        return serde_json::to_string(&results).unwrap();
-    }
-    if let Ok(grids) = grids_arr {
-        for grid in grids {
-            let ctx = grid.getText();
-            let r = Result {
-                result: true,
-                content: ctx.to_string(),
-            };
-            results.push(r);
-        }
-    }
-    let results_str = serde_json::to_string(&results).unwrap();
-    return results_str;
+    return parse_images_with_paths(
+        file_name.into_os_string().into_string().unwrap(),
+        Some("all".to_string()),
+    )
+    .await;
 }
